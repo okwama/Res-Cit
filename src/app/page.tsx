891dc34c1ed59966d43f4a7540e73324d3e45afa
Benjamin Okwama <bennjiokwam@gmail.com>
2024-10-26 02:43:32 +0300

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      {/* <Special/> */}
+      <Special/> 
       <Featured/>
-      {/* <Offer/> */}
+      <Offer/>
     </main>
   )
 }
