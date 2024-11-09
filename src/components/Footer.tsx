31565b45becd3fdda6d518ab3d64c390f6616d5c
Benjamin Okwama <bennjiokwam@gmail.com>
2024-11-09 14:05:04 +0300

    ftx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from 'next/image';
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-indigo-900 flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-indigo-900 flex items-center justify-between '>
   <Link href="/"> 
   <Image src="https://ik.imagekit.io/bja2qwwdjjy/Artboard%201@1.5x_sHGlLMjLB.png?updatedAt=1730979703401" alt="" className="h-8" width="50" height="100" />
   </Link>
